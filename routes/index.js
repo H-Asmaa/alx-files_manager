73df0ed9c051bd6138215ae600dfab9f7f634de2
H-Asmaa <asmaehadar32@gmail.com>
2024-03-09 15:18:41 +0100

    Committing a new route added in the index.
    The route is dedicated for the post user action.

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', (req, res) => AppController.getStatus(req, res));
 router.get('/stats', (req, res) => AppController.getStats(req, res));
+router.post('/users', (req, res) => UsersController.postUser(req, res));
 
-export default (app) => {
-  app.use(router);
-};
+export default router;
