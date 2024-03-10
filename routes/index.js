57574d571b54e9cf422e59a8443496c7cf7a8759
H-Asmaa <asmaehadar32@gmail.com>
2024-03-10 20:27:57 +0000

    Committing tree new routes hooray.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
-// import AuthController from '../controllers/AuthController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
@@ -10,8 +10,8 @@ router.get('/status', (req, res) => AppController.getStatus(req, res));
 router.get('/stats', (req, res) => AppController.getStats(req, res));
 router.post('/users', (req, res) => UsersController.postUser(req, res));
 
-// router.get('/connect', (req, res) => AuthController.getConnect(req, res));
-// router.get('/disconnect', (req, res) => AuthController.getDisconnect(req, res));
-// router.get('/users/me ', (req, res) => UsersController.getMe(req, res));
+router.get('/connect', (req, res) => AuthController.getConnect(req, res));
+router.get('/disconnect', (req, res) => AuthController.getDisconnect(req, res));
+router.get('/users/me', (req, res) => UsersController.getMe(req, res));
 
 export default router;
