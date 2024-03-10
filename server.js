570acf7e4570b779f1469562c46009de0c011508
H-Asmaa <asmaehadar32@gmail.com>
2024-03-10 17:43:15 +0000

    Committing a trial to fix the requirement fail.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import router from './routes/index';
 
 const server = express();
-const port = parseInt(process.env.PORT, 10) || 5000;
+const port = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(router);
