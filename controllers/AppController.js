5030a2cb39493ce0155372e38fac5e42d443d544
H-Asmaa <asmaehadar32@gmail.com>
2024-03-11 03:09:16 +0000

    Committing the requirement fail still persue.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,18 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  static async getStatus(req, res) {
+const AppController = {
+  async getStatus(req, res) {
     const redis = await redisClient.isAlive();
     const db = await dbClient.isAlive();
     return res.status(200).json({ redis, db });
-  }
+  },
 
-  static async getStats(req, res) {
+  async getStats(req, res) {
     const dbUsers = await dbClient.nbUsers();
     const dbFilesCount = await dbClient.nbFiles();
     return res.status(200).json({ users: dbUsers, files: dbFilesCount });
-  }
-}
+  },
+};
 
-module.exports = AppController;
+export default AppController;
