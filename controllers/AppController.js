3af57dbf5c7ec601eadcb673b88a054dc9ec3401
H-Asmaa <asmaehadar32@gmail.com>
2024-03-11 03:34:50 +0000

    Committing older version since it turned out that the problem is not because I didn't use a class.

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,18 +1,18 @@
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-class AppController {
-  static getStatus(req, res) {
-    const redis = redisClient.isAlive();
-    const db = dbClient.isAlive();
+const AppController = {
+  async getStatus(req, res) {
+    const redis = await redisClient.isAlive();
+    const db = await dbClient.isAlive();
     return res.status(200).json({ redis, db });
-  }
+  },
 
-  static async getStats(req, res) {
+  async getStats(req, res) {
     const dbUsers = await dbClient.nbUsers();
     const dbFilesCount = await dbClient.nbFiles();
     return res.status(200).json({ users: dbUsers, files: dbFilesCount });
-  }
-}
+  },
+};
 
 export default AppController;
