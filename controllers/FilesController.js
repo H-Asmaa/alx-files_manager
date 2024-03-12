1ffe7f510303cd01a92bb9f1ef0047af2832efe9
H-Asmaa <asmaehadar32@gmail.com>
2024-03-12 03:10:37 +0000

    Committing fix for the output fail in the image upload case.
    Upon long investigation turned out that the flow of the program stops at the line where we get the file with the parentId if the parentId is different than zero.
    
    Long story short the mistake was simple. I had import ObjectId from 'mongodb';
    instead of import { ObjectId } from 'mongodb';. Call me crazy.

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -1,4 +1,4 @@
-import ObjectId from 'mongodb';
+import { ObjectId } from 'mongodb';
 import { v4 as uuidv4 } from 'uuid';
 import fs from 'fs';
 import dbClient from '../utils/db';
@@ -7,32 +7,39 @@ import redisClient from '../utils/redis';
 class FilesController {
   static async postUpload(req, res) {
     const token = req.headers['x-token'];
+    // console.log('token: '+token);
     if (!token) return res.status(401).send({ error: 'Unauthorized' });
 
     const redisUserId = await redisClient.get(`auth_${token}`);
+    // console.log('userId '+redisUserId);
     if (!redisUserId) return res.status(401).send({ error: 'Unauthorized' });
 
     const { name } = req.body;
+    // console.log('name: '+name);
     if (!name) return res.status(400).send({ error: 'Missing name' });
 
     const { type } = req.body;
+    // console.log('type: '+type);
     const fileTypes = new Set(['file', 'folder', 'image']);
     if (!type || !fileTypes.has(type)) return res.status(400).send({ error: 'Missing type' });
 
     const { data } = req.body;
+    // console.log('data: '+data);
     if (!data && type !== 'folder') return res.status(400).send({ error: 'Missing data' });
 
     const parentId = req.body.parentId || 0;
-
+    // console.log('parentId: '+parentId);
     if (parentId) {
       const file = await dbClient.db
         .collection('files')
         .findOne({ _id: ObjectId(parentId) });
+      // console.log('file: '+file);
       if (!file) return res.status(400).send({ error: 'Parent not found' });
       if (file.type !== 'folder') return res.status(400).send({ error: 'Parent is not a folder' });
     }
 
     const isPublic = req.body.isPublic || false;
+    // console.log(isPublic);
 
     const fileToStore = {
       userId: redisUserId,
@@ -41,8 +48,10 @@ class FilesController {
       isPublic,
       parentId,
     };
+    // console.log('fileToStore: '+fileToStore);
 
     if (type === 'folder') {
+      // console.log('type is folder.');
       await dbClient.db.collection('files').insertOne(fileToStore);
       return res.status(201).send({
         id: fileToStore._id,
@@ -55,23 +64,28 @@ class FilesController {
     }
 
     const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+    // console.log('folderPath :'+folderPath);
     const filePath = `${folderPath}/${uuidv4()}`;
+    // console.log('filePath: '+filePath);
 
     if (!fs.existsSync(folderPath)) {
-      fs.mkdirSync(folderPath, { recursive: true }, (error) => {
+      // console.log('About to create the directory.');
+      await fs.mkdirSync(folderPath, { recursive: true }, (error) => {
         if (!error) return true;
         return false;
       });
     }
 
     const fileData = Buffer.from(data, 'base64');
+    // console.log('fileData: '+fileData);
 
-    fs.writeFile(filePath, fileData, (error) => {
+    await fs.writeFile(filePath, fileData, (error) => {
       if (!error) return true;
       return false;
     });
 
     fileToStore.localPath = filePath;
+    // console.log('fileToStore: '+fileToStore);
     await dbClient.db.collection('files').insertOne(fileToStore);
 
     return res.status(201).send({
